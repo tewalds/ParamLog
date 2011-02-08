3ebe2e74f1c14fda9744ef914a80b727ff9356f1
Timo Ewalds <timo@ewalds.ca>
2011-02-07 21:16:57 -0700

    spacing cleanup...

--- www/index.php
+++ www/index.php
@@ -7,13 +7,13 @@ include("include/lib.php");
 
 $router = new PHPRouter();
 
-$router->add("GET", "/", "home.php", "home", null);
+$router->add("GET", "/",     "home.php", "home", null);
 $router->add("GET", "/info", "home.php", "info", null);
-$router->add("GET", "/get", "home.php", "get", array("test" => "string"));
+$router->add("GET", "/get",  "home.php", "get", array("test" => "string"));
 $router->add("POST","/post", "home.php", "post", array("test" => "string"));
 
-$router->add("GET", "/results", "results.php", "showresults", array("baselines" => "array", "times" => "array", "players" => "array", "errorbars" => "bool", "simpledata" => "bool", "data" => "bool", "scale" => "bool"));
-$router->add("GET", "/results/hosts", "results.php", "gethosts", null);
+$router->add("GET", "/results",        "results.php", "showresults", array("baselines" => "array", "times" => "array", "players" => "array", "errorbars" => "bool", "simpledata" => "bool", "data" => "bool", "scale" => "bool"));
+$router->add("GET", "/results/hosts",  "results.php", "gethosts", null);
 $router->add("GET", "/results/recent", "results.php", "getrecent", null);
 
 $router->add("GET",  "/players",        "players.php", "players_list",   null);
@@ -21,8 +21,8 @@ $router->add("POST", "/players/route",  "players.php", "players_route",  array("
 $router->add("POST", "/players/update", "players.php", "players_update", array("players" => "array", "names" => "array", "weights" => "array", "params" => "array"));
 $router->add("POST", "/players/add",    "players.php", "players_add",    array("names" => "array", "weights" => "array", "params" => "array"));
 
-$router->add("GET","/api/getwork", "api.php", "getwork", null);
-$router->add("POST","/api/submit", "api.php", "submit", array("baseline" => "int", "player" => "int", "size" => "int", "time" => "int", "outcome" => "int", "log" => "string"));
+$router->add("GET", "/api/getwork", "api.php", "getwork", null);
+$router->add("POST","/api/submit",  "api.php", "submit", array("baseline" => "int", "player" => "int", "size" => "int", "time" => "int", "outcome" => "int", "log" => "string"));
 
 
 $route = $router->route();
