157bcabd7e98e334a88cdc9e070bcc2fab4ad613
Timo Ewalds <timo@ewalds.ca>
2011-03-16 13:27:27 -0600

    Improve static file serving, reduce code duplication, give good cache headers and return 304 not modified when appropriate

--- www/pages/static.php
+++ www/pages/static.php
@@ -3,42 +3,25 @@
 $filetypes = array(
 	".html" => "text/html",
 	".css"  => "text/css",
-	".js"   => "application/js",
+	".js"   => "text/javascript",
 	".png"  => "image/png",
 	".jpeg" => "image/jpeg",
 	".jpg"  => "image/jpeg",
 	".gif"  => "image/gif",
 );
 
-
 function staticcontent($data, $user, $url){
-	global $filetypes;
-
-	if(substr($url, 0, 8) != "/static/")
-		redirect("/404?referer=$url");
-
-	$url = str_replace("../", "", $url);
-	$url = str_replace("./", "", $url);
-
-	$file = substr($url, 1);
-
-	if(!file_exists($file))
-		redirect("/404?referer=$url");
-
-
-	$fileending = substr($file, strrpos($file,'.'));
-
-	header("Content-Type: " . $filetypes[$fileending]);
-
-	readfile($file);
-
-	return false;
+	return servestatic($data, $user, $url, "/static/");
 }
 
 function staticimages($data, $user, $url){
+	return servestatic($data, $user, $url, "/images/");
+}
+
+function servestatic($data, $user, $url, $base){
 	global $filetypes;
 
-	if(substr($url, 0, 8) != "/images/")
+	if(substr($url, 0, strlen($base)) != $base)
 		redirect("/404?referer=$url");
 
 	$url = str_replace("../", "", $url);
@@ -46,12 +29,25 @@ function staticimages($data, $user, $url){
 
 	$file = substr($url, 1);
 
-
 	if(!file_exists($file))
 		redirect("/404?referer=$url");
 
+
+	$mtime = filemtime($file);
+
+	if(isset($_SERVER['HTTP_IF_MODIFIED_SINCE'])){
+		$iftime = strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']);
+		if($mtime <= $iftime){
+			header($_SERVER["SERVER_PROTOCOL"] . " 304 Not Modified");
+			return false;
+		}
+	}
+
 	$fileending = substr($file, strrpos($file,'.'));
-	header("Content-Type: " . $filetypes[$fileending]);
+	header("content-type: " . $filetypes[$fileending]);
+	header("expires: " . date(DATE_RFC822, time()+86400*7));
+	header("last-modified: " . date(DATE_RFC822, $mtime));
+	header("cache-control: public, max-age=31536000");
 
 	readfile($file);
 
