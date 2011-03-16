388424164e6822727ee32bc9c0c9c163198ab0c8
Timo Ewalds <timo@ewalds.ca>
2011-03-16 01:01:33 -0600

    Point to the web-accessible version instead of the local dev version

--- bin/worker.rb
+++ bin/worker.rb
@@ -7,7 +7,7 @@
 	}
 
 	$parallel = 1;
-	$url = "http://paramlog.local.ewalds.ca";
+	$url = "http://paramlog.ewalds.ca";
 	$apikey = "1bab1eae9a58b147de6f7518c6644a38"
 	$benchplayer = Castro
 	$referee = Castro
