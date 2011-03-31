53211af140a69415363967ec982872758a13c853
Timo Ewalds <timo@ewalds.ca>
2011-03-30 23:13:03 -0600

    Modify the gambler adapter to use a config file for parameters

--- bin/players/gambler.rb
+++ bin/players/gambler.rb
@@ -3,18 +3,43 @@
 require 'players/gtpplayer.rb'
 
 class Gambler < GTPPlayer
-	@path = "~/Desktop/gambler"
+	@path = "/home/timo/Desktop/gambler"
 	@exec = "gamblerh-w32.exe"
 	@sides = ['none', 'white', 'black', 'draw']
 
+
 	def initialize
-		@gtp = GTPClient.new("#{path}/#{exec}", "\r\n")
+		@time = {:move => 0, :game => 120, :sims => 0}
+		@params = ""
+		@gtp = nil
+	end
+	def boardsize(size)
+		@boardsize = size
 	end
 	def time(move, game, sims)
-		@gtp.cmd "time_settings #{game} #{move} #{sims}"
+		@time[:move] = move
+		@time[:game] = game
+		@time[:sims] = sims
 	end
 	def params(param)
-#		@gtp.cmd "player_params #{param}"
+		@params += param + "\r\n"
+	end
+	def start
+		@filename = rand 1000000000
+		config = File.open("#{path}/gambler.ini"){|f| f.read }
+		config += "\r\n" + @params
+		File.open("/tmp/gambler.#{@filename}.ini", "w"){|f| f.write config }
+
+		cmd = "#{path}/#{exec} /tmp/gambler.#{@filename}"
+		puts "> #{cmd}"
+		@gtp = GTPClient.new(cmd, "\r\n")
+		@gtp.cmd "boardsize #{@boardsize}"
+		@gtp.cmd "time_settings #{@time[:game]} #{@time[:move]} 1"
+	end
+	def quit
+		@gtp.cmd "quit"
+		@gtp.close
+		`rm -f /tmp/gambler.#{@filename}.ini`
 	end
 	def winner
 		return 0
